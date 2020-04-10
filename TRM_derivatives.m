5aa15c1d1ee7ae2623767bf4de02b480247e3ddf
yaoganchenchi <yaoganchenchi@gmail.com>
2020-04-10 17:46:40 -0400

    Update TRM_derivatives.m

--- TRM_derivatives.m
+++ TRM_derivatives.m
@@ -78,12 +78,12 @@ dTs_1st_dalbedo  = diff(Ts_1st, albedo, 1);
 dTs_1st_dra      = diff(Ts_1st, ra, 1);
 dTs_1st_drs      = diff(Ts_1st, rs, 1);
 
-% 2nd order derivative for FOTSE LST model
+% 2nd order derivatives for FOTSE LST model
 ddTs_1st_ddalbedo = diff(Ts_1st, albedo, 2);
 ddTs_1st_ddra     = diff(Ts_1st, ra, 2);
 ddTs_1st_ddrs     = diff(Ts_1st, rs, 2);
 
-% degree 2 cross-order derivative for FOTSE LST model
+% degree 2 cross-order derivatives for FOTSE LST model
 ddTs_1st_dalbedo_dra = diff(diff(Ts_1st, albedo, 1), ra, 1);
 ddTs_1st_dra_drs     = diff(diff(Ts_1st, ra, 2), rs, 1);
 ddTs_1st_drs_dalebdo = diff(diff(Ts_1st, rs, 1), albedo, 1);
@@ -102,12 +102,12 @@ dTs_2nd_dalbedo  = diff(Ts_2nd, albedo, 1);
 dTs_2nd_dra      = diff(Ts_2nd, ra, 1);
 dTs_2nd_drs      = diff(Ts_2nd, rs, 1);
 
-% 2nd order derivative for FOTSE LST model
+% 2nd order derivatives for FOTSE LST model
 ddTs_2nd_ddalbedo = diff(Ts_2nd, albedo, 2);
 ddTs_2nd_ddra     = diff(Ts_2nd, ra, 2);
 ddTs_2nd_ddrs     = diff(Ts_2nd, rs, 2);
 
-% degree 2 cross-order derivative for FOTSE LST model
+% degree 2 cross-order derivatives for FOTSE LST model
 ddTs_2nd_dalbedo_dra = diff(diff(Ts_2nd, albedo, 1), ra, 1);
 ddTs_2nd_dra_drs     = diff(diff(Ts_2nd, ra, 2), rs, 1);
 ddTs_2nd_drs_dalebdo = diff(diff(Ts_2nd, rs, 1), albedo, 1);
